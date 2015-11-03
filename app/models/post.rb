f2cc6c4d278e7442dd673583268214963c103a3f
Alexey Fomichev <thomas.web20@gmail.com>
2015-11-03 12:09:42 +0300

    Add association between post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,13 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+
 	has_many :categories_posts
 	has_many :categories, through: :categories_posts
+
 	has_many :tags_posts
 	has_many :tags, through: :tags_posts
+
+	belongs_to :user
 
 	validates :title, :body, presence: true
 end
